d52053f59615c65635587578ad8babd0e5187a63
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 19:17:14 +0600

    Added getter and setter in the Club.java

--- Main/src/model/Club.java
+++ Main/src/model/Club.java
@@ -18,4 +18,41 @@ public class Club {
         this.members = members;
         this.events=events;
     }
+
+    public long getClubId(){
+        return clubId;
+    }
+    public void setClubId(long clubId){
+        this.clubId = clubId;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public String getPresident(){
+        return president;
+    }
+    public void setPresident(String president){
+        this.president = president;
+    }
+    public String getMembers(){
+        return members;
+    }
+    public void setMembers(String members){
+        this.members = members;
+    }
+    public String getEvents(){
+        return events;
+    }
+    public void setEvents(String events){
+        this.events = events;
+    }
 }
