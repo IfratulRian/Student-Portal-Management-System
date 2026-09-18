96aea693e67691935d049161533f14cd4350a1b9
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 19:01:28 +0600

    getter and setter for Student.java

--- Main/src/model/Student.java
+++ Main/src/model/Student.java
@@ -15,4 +15,29 @@ public class Student extends Person{
         this.cgpa = cgpa;
         this.points = points;
     }
+
+    public String getDepartment(){
+        return department;
+    }
+    public void setDepartment(String department){
+        this.department = department;
+    }
+    public int getSemester(){
+        return semester;
+    }
+    public void setSemester(int semester){
+        this.semester = semester;
+    }
+    public double getCgpa(){
+        return cgpa;
+    }
+    public void setCgpa(double cgpa){
+        this.cgpa = cgpa;
+    }
+    public int getPoints(){
+        return points;
+    }
+    public void setPoints(int points){
+        this.points = points;
+    }
 }
