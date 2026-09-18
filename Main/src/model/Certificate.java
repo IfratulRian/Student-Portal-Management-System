92ee61a9285a2bc4b5c04e5811a86c0634ecde3b
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 21:25:28 +0600

    Added getter and setter in the Certificate.java

--- Main/src/model/Certificate.java
+++ Main/src/model/Certificate.java
@@ -2,18 +2,48 @@ package model;
 
 public class Certificate {
     private long certificateId;
-    private String student;
-    private String event;
+    private Student student;
+    private Event event;
     private String type;
     private String issueDate;
 
-    public Certificate(long certificateId, String student, String event,
+    public Certificate(long certificateId, Student student, Event event,
     String type, String issueDate){
 
         this.certificateId = certificateId;
         this.student = student;
         this.event = event;
         this.type = type;
+        this.issueDate = issueDate;
+    }
+    public long getCertificateId(){
+        return certificateId;
+    }
+    public void setCertificateId(long certificateId){
+        this.certificateId = certificateId;
+    }
+    public Student getStudent(){
+        return student;
+    }
+    public void setStudent(Student student){
+        this.student = student;
+    }
+    public Event getEvent(){
+        return event;
+    }
+    public void setEvent(Event event){
+        this.event = event;
+    }
+    public String getType(){
+        return type;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getIssueDate(){
+        return issueDate;
+    }
+    public void setIssueDate(String issueDate){
         this.issueDate = issueDate;
     }
 }
