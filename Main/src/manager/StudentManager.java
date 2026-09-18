4a737e773fda547d5e7f6bd994126117605bb2ee
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 22:20:35 +0600

    added starter

--- Main/src/manager/StudentManager.java
+++ Main/src/manager/StudentManager.java
@@ -1,4 +1,15 @@
 package manager;
 
 public class StudentManager {
+    public void start(){
+        System.out.println("\n===== Student Management =====");
+        System.out.println("1. Add Student");
+        System.out.println("2. View Students");
+        System.out.println("3. Search Student");
+        System.out.println("4. Update Student");
+        System.out.println("5. Delete Student");
+        System.out.println("0. Back");
+
+        System.out.println("Student Manager Started");
+    }
 }
