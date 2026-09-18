d76967ea0f2c98ea026e494d283ab28c91c3b671
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 22:03:58 +0600

    Added the starter

--- Main/src/manager/ClubManager.java
+++ Main/src/manager/ClubManager.java
@@ -1,4 +1,8 @@
 package manager;
 
 public class ClubManager {
+    public void start(){
+        System.out.println("\n===== Club Management =====");
+        System.out.println("Club Manager Started");
+    }
 }
