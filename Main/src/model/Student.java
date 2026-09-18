92c76503583a50bbc4bedb3160b4faad34343aa7
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 15:12:10 +0600

    Created constructor for Student.java

--- Main/src/model/Student.java
+++ Main/src/model/Student.java
@@ -1,9 +1,20 @@
 package model;
 
+import javax.swing.*;
+
 public class Student extends Person{
-    long StudenId;
+    long StudentId;
     String department;
     int semester;
     double cgpa;
     int points; //Credits
+
+    public Student(long id, String name, String email, String phone, long adminId, String role){
+        super(id, name, email, phone);
+        this.StudentId = StudentId;
+        this.department = department;
+        this.semester = semester;
+        this.cgpa = cgpa;
+        this.points = points;
+    }
 }
