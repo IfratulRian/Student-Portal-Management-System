9ceb69021b254a93a8ee58e1ddf6a5c4aad990e6
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 12:28:03 +0600

    Added attributes of Registration.java

--- Main/src/model/Registration.java
+++ Main/src/model/Registration.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Registration {
+    long registrationId;
+    String student;
+    String event;
+    String regestrationDate;
+    String status;
+    double attendance;
+    int score;
 }
