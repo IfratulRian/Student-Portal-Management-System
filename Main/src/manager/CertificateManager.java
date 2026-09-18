d4a77a2663de4814a134fe1afb1036502d86497f
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 22:03:13 +0600

    Added the start part

--- Main/src/manager/CertificateManager.java
+++ Main/src/manager/CertificateManager.java
@@ -1,4 +1,8 @@
 package manager;
 
 public class CertificateManager {
+    public void start(){
+        System.out.println("\n===== Certificate Management =====");
+        System.out.println("Certificate Manager Started");
+    }
 }
