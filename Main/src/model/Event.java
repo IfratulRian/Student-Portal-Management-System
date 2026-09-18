2d28dbececdb50403fb121560196f8acd2fdf225
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 19:39:24 +0600

    Added getter anb setter for Event.java

--- Main/src/model/Event.java
+++ Main/src/model/Event.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Event {
     private long eventId;
     private String name;
@@ -8,12 +10,12 @@ public class Event {
     private String date;
     private int maxParticipants;
     private int registrationFee;
-    private String organizer;
-    private String participants;
+    private Club organizer;
+    private ArrayList<Student> participants;
 
     public Event(long eventId, String name, String type, String date,
-        int maxParticipants, int registrationFee, String organizer,
-        String participants){
+        int maxParticipants, int registrationFee, Club organizer,
+                 ArrayList<Student> participants){
 
         this.eventId = eventId;
         this.name = name;
@@ -22,6 +24,52 @@ public class Event {
         this.maxParticipants = maxParticipants;
         this.registrationFee = registrationFee;
         this.organizer = organizer;
+        this.participants = participants;
+    }
+
+    public long getEventId(){
+        return eventId;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getType(){
+        return type;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getDate(){
+        return date;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public int getMaxParticipants(){
+        return maxParticipants;
+    }
+    public void setMaxParticipants(int maxParticipants){
+        this.maxParticipants = maxParticipants;
+    }
+    public int getRegistrationFee(){
+        return registrationFee;
+    }
+    public void setRegistrationFee(int registrationFee){
+        this.registrationFee = registrationFee;
+    }
+    public Club getOrganizer(){
+        return organizer;
+    }
+    public void setOrganizer(Club organizer){
+        this.organizer = organizer;
+    }
+    public ArrayList<Student>getParticipants(){
+        return participants;
+    }
+    public void setParticipants(ArrayList<Student>participants){
         this.participants = participants;
     }
 }
