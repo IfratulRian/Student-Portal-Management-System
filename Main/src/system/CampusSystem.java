53ac9807a61a6e79cc80a9712e9923782f2f25c9
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 21:42:14 +0600

    Main Done

--- Main/src/system/CampusSystem.java
+++ Main/src/system/CampusSystem.java
@@ -1,5 +1,11 @@
 package system;
+import java.util.Scanner;
 
 public class CampusSystem {
+    private static Scanner sc = new Scanner(System.in);
 
+    public static void start(){
+        int choice = sc.nextInt();
+        while(choice != 0)
+    }
 }
