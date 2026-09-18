a7464b644dc7fdecc17c8972e560590b56198a14
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 19:20:04 +0600

    Changed some declartion

--- Main/src/model/Club.java
+++ Main/src/model/Club.java
@@ -1,22 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Club {
     private long clubId;
     private String name;
     private String description;
-    private String president;
-    private String members;
-    private String events;
+    private Student president;
+    private ArrayList<Student> members;
+    private ArrayList<Event> events;
 
     public Club(long clubId, String name, String description,
-        String president, String members, String events){
+                Student president, ArrayList<Student> members,
+                ArrayList<Event> events){
 
         this.clubId = clubId;
         this.name = name;
         this.description = description;
         this.president = president;
         this.members = members;
-        this.events=events;
+        this.events = events;
     }
 
     public long getClubId(){
@@ -37,22 +40,22 @@ public class Club {
     public void setDescription(String description){
         this.description = description;
     }
-    public String getPresident(){
+    public Student getPresident(){
         return president;
     }
-    public void setPresident(String president){
+    public void setPresident(Student president){
         this.president = president;
     }
-    public String getMembers(){
+    public ArrayList<Student> getMembers(){
         return members;
     }
-    public void setMembers(String members){
+    public void setMembers(ArrayList<Student> members){
         this.members = members;
     }
-    public String getEvents(){
+    public ArrayList<Event> getEvents(){
         return events;
     }
-    public void setEvents(String events){
+    public void setEvents(ArrayList<Event> events){
         this.events = events;
     }
 }
