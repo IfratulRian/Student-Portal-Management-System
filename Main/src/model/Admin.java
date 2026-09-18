97079bb24ec1c7294c281356d4b5d9d7a26b783f
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 19:07:00 +0600

    Removed AdminId

--- Main/src/model/Admin.java
+++ Main/src/model/Admin.java
@@ -1,22 +1,14 @@
 package model;
 
 public class Admin extends Person{
-    private long adminId;
     private String role; //Designation.
     /* Like: SUPER_ADMIN, EVENT_MANAGER, CLUB_MANAGER*/
 
-    public Admin(long id, String name, String email, String phone, long adminId, String role){
+    public Admin(long id, String name, String email, String phone, String role){
         super(id, name, email, phone);
-        this.adminId = adminId;
         this.role = role;
     }
 
-    public long getAdminId(){
-        return adminId;
-    }
-    public void setAdminId(long adminId){
-        this.adminId = adminId;
-    }
     public String getRole(){
         return role;
     }
