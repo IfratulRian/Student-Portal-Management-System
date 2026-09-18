4fce909849b1c6d1e51702aa72863b6be9d58ebf
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 22:05:39 +0600

    Added the starter

--- Main/src/manager/EventManager.java
+++ Main/src/manager/EventManager.java
@@ -1,7 +1,6 @@
 package manager;
 
 public class EventManager {
-
     public void start(){
         System.out.println("\n===== Event Management =====");
         System.out.println("Event Manager Started");
--- Main/src/manager/RegistrationManager.java
+++ Main/src/manager/RegistrationManager.java
@@ -1,4 +1,8 @@
 package manager;
 
 public class RegistrationManager {
+    public void start(){
+        System.out.println("\n===== Registration Management =====");
+        System.out.println("Registration Manager Started");
+    }
 }
