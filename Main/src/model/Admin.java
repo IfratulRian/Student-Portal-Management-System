d68e42987aef17b4b9e626c18e5a6e72a37f7b04
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-18 14:59:09 +0600

    Created constructor for Admin.java and Person.java

--- Main/src/model/Admin.java
+++ Main/src/model/Admin.java
@@ -4,4 +4,10 @@ public class Admin extends Person{
     long adminId;
     String role; //Designation.
     /* Like: SUPER_ADMIN, EVENT_MANAGER, CLUB_MANAGER*/
+
+    public Admin(long id, String name, String email, String phone, long adminId, String role){
+        super(id, name, email, phone);
+        this.adminId = adminId;
+        this.role = role;
+    }
 }
--- Main/src/model/Person.java
+++ Main/src/model/Person.java
@@ -1,8 +1,15 @@
 package model;
 
-abstract class Person {
+public abstract class Person {
     long id;
     String name;
     String email;
     String phone;
+
+    public Person(long id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
 }
