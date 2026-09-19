2a33ddf9d9f665ec6e8e43a25bafce8db3c3009e
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-19 12:06:13 +0600

    FileManager.java updated

--- Main/src/util/FileManager.java
+++ Main/src/util/FileManager.java
@@ -1,4 +1,34 @@
 package util;
+import java.io.*;
+import java.util.ArrayList;
 
 public class FileManager {
+    public static void write(String fileName, ArrayList<String> data){
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            for(String line : data){
+                writer.write(line);
+                writer.newLine();
+            }
+            writer.close();
+        }
+        catch(IOException e){
+            System.out.println("Error writing to file.");
+        }
+    }
+    public static ArrayList<String> read(String fileName){
+        ArrayList<String> data = new ArrayList<>();
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while((line = reader.readLine()) != null){
+                data.add(line);
+            }
+            reader.close();
+        }
+        catch(IOException e){
+            System.out.println("Error reading file.");
+        }
+        return data;
+    }
 }
