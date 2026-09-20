c4b9a9e0be476816914da72a3111203b561355b8
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 19:13:36 +0600

    Added imported files

--- Main/src/manager/CertificateManager.java
+++ Main/src/manager/CertificateManager.java
@@ -1,5 +1,13 @@
 package manager;
 
+import model.Certificate;
+import model.Event;
+import model.Student;
+import util.FileManager;
+
+import java.util.ArrayList;
+import java.util.*;
+
 public class CertificateManager {
     public void start(){
         System.out.println("\n===== Certificate Management =====");
