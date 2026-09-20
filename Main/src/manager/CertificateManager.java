a16bb2b602c240e57a395fc863e475dcaa918979
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 19:26:58 +0600

    Added visible part

--- Main/src/manager/CertificateManager.java
+++ Main/src/manager/CertificateManager.java
@@ -9,8 +9,29 @@ import java.util.ArrayList;
 import java.util.*;
 
 public class CertificateManager {
+    private Scanner sc = new Scanner(System.in);
+    private ArrayList<Certificate>certificates = new ArrayList<>();
+    private ArrayList<Student>students = new ArrayList<>();
+    private ArrayList<Event>events = new ArrayList<>();
+
     public void start(){
-        System.out.println("\n===== Certificate Management =====");
-        System.out.println("Certificate Manager Started");
+        int choice;
+
+        do{
+            System.out.println("\n===== Certificate Management =====");
+            System.out.println("1. Issue Certificate");
+            System.out.println("2. View Certificates");
+            System.out.println("3. Search Certificate");
+            System.out.println("4. Update Certificate");
+            System.out.println("5. Delete Certificate");
+            System.out.println("0. Back");
+            System.out.print("Enter your choice: ");
+
+            choice = sc.nextInt();
+
+            switch(choice){
+
+            }while(choice != 0);
+        }
     }
 }
