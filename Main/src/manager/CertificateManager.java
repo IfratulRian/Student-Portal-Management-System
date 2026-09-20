86878455b01439a5ce7a645ce5ae0765b74225cc
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-20 20:10:36 +0600

    Almost all done for CertificateManager.java

--- Main/src/manager/CertificateManager.java
+++ Main/src/manager/CertificateManager.java
@@ -30,8 +30,122 @@ public class CertificateManager {
             choice = sc.nextInt();
 
             switch(choice){
+                case 1:
+                    issueCertificate();
+                    break;
+                case 2:
+                    viewCertificates();
+                    break;
+                case 3:
+                    searchCertificate();
+                    break;
+                case 4:
+                    updateCertificate();
+                    break;
+                case 5:
+                    deleteCertificate();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }while(choice != 0);
+    }
 
-            }while(choice != 0);
+    private void saveCertificates(){
+        ArrayList<String> data = new ArrayList<>();
+
+        for(Certificate certificate : certificates){
+            String line =
+                    certificate.getCertificateId() + "|" +
+                            certificate.getStudent().getId() + "|" +
+                            certificate.getEvent().getEventId() + "|" +
+                            certificate.getType() + "|" +
+                            certificate.getIssueDate();
+            data.add(line);
         }
+        FileManager.write("data/certificates.txt", data);
+    }
+
+    private void issueCertificate(){
+        System.out.print("Enter Certificate ID: ");
+        long certificateId = sc.nextLong();
+
+        System.out.print("Enter Student ID: ");
+        long studentId = sc.nextLong();
+
+        Student student = null;
+        for(Student s : students){
+            if(s.getId() == studentId){
+                student = s;
+                break;
+            }
+        }
+
+        if(student == null){
+            System.out.println("Student not found!");
+            return;
+        }
+
+        System.out.print("Enter Event ID: ");
+        long eventId = sc.nextLong();
+        sc.nextLine();
+
+        Event event = null;
+        for(Event e : events){
+            if(e.getEventId() == eventId){
+                event = e;
+                break;
+            }
+        }
+        if(event == null){
+            System.out.println("Event not found!");
+            return;
+        }
+
+        System.out.print("Enter Certificate Type: ");
+        String type = sc.nextLine();
+
+        System.out.print("Enter Issue Date: ");
+        String issueDate = sc.nextLine();
+
+        Certificate certificate = new Certificate(
+                certificateId,
+                student,
+                event,
+                type,
+                issueDate
+        );
+        certificates.add(certificate);
+        saveCertificates();
+        System.out.println("Certificate issued successfully!");
+    }
+
+    private void viewCertificates(){
+        if(certificates.isEmpty()){
+            System.out.println("No certificates found.");
+            return;
+        }
+
+        for(Certificate certificate : certificates){
+            System.out.println(
+                    certificate.getCertificateId() + " | " +
+                            certificate.getStudent().getName() + " | " +
+                            certificate.getEvent().getName() + " | " +
+                            certificate.getType() + " | " +
+                            certificate.getIssueDate()
+            );
+        }
+    }
+
+    private void searchCertificate(){
+
+    }
+    private void updateCertificate(){
+
+    }
+    private void deleteCertificate(){
+
     }
 }
