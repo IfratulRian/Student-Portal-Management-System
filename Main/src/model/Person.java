eb9a65bfbef7b0229104a3dae1f43ae2db7ad56a
Asif-1217 <asifalmasud17@gmail.com>
2026-09-22 10:23:52 +0600

    Updated Person and Student

--- Main/src/model/Person.java
+++ Main/src/model/Person.java
@@ -1,47 +1,82 @@
 package model;
 
+import java.time.LocalDate;
+
 public abstract class Person {
     private long id;
     private String name;
     private String email;
     private String phone;
+    private String address;
+    private LocalDate dateOfBirth;
 
-    public Person(long id, String name, String email, String phone) {
+    public Person(long id, String name, String email, String phone,
+                  String address, LocalDate dateOfBirth) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
     }
 
-    public long getId(){
+    public long getId() {
         return id;
     }
 
-    public void setId(long id){
+    public void setId(long id) {
         this.id = id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email){
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPhone(){
+    public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone){
+    public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
     }
 }
